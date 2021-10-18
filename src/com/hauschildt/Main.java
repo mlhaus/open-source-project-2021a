54cf198cea41a20a35badfd5535b4d6eece64c25
Christopher T Repko <k0804000@student.kirkwood.edu>
2021-10-18 13:08:33 -0500

    Attempt to dodge merge conflicts as a test.

--- src/com/hauschildt/Main.java
+++ src/com/hauschildt/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Order order = new Order();
         order.addItem(PizzaItem.PEPPERONI.getItem());
+        order.addItem(PizzaItem.MEAT.getItem());
         order.addItem(PizzaItem.SAUSAGE.getItem());
         order.addItem(PizzaItem.CHEESE.getItem());
         order.addItem(PizzaItem.THE_WORKS.getItem());
-        order.addItem(PizzaItem.MEAT.getItem());
         System.out.println(order.getNumberOfItems());
         System.out.println(order.getItem(0).getName());
         System.out.println(order.getItem(1).getName());
