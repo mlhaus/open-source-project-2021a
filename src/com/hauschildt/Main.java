a044129be260808befe2bb623e0f29f581270e80
Andrew Aldrich <andrewaldrich1993@gmail.com>
2021-10-30 20:18:36 -0500

    Added BBQ chicken pizza

--- src/com/hauschildt/Main.java
+++ src/com/hauschildt/Main.java
@@ -10,11 +10,13 @@ public class Main {
         order.addItem(PizzaItem.SAUSAGE.getItem());
         order.addItem(PizzaItem.CHEESE.getItem());
         order.addItem(PizzaItem.THE_WORKS.getItem());
+        order.addItem(PizzaItem.BBQ_CHICKEN.getItem());
         System.out.println(order.getNumberOfItems());
         System.out.println(order.getItem(0).getName());
         System.out.println(order.getItem(1).getName());
         System.out.println(order.getItem(2).getName());
         System.out.println(order.getItem(3).getName());
+        System.out.println(order.getItem(4).getName());
         // Hello
     }
 }
